c9a69531bec1e475e3e76a8e770de80f5a2b3c35
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-09-13 10:27:25 -0500

    Finish impl

--- src/myreverse.lisp
+++ src/myreverse.lisp
@@ -1,11 +1,13 @@
-; Jerred Shepherd`
+; Jerred Shepherd
 (defun myreverse (list) 
     (if (null list)
         ()
-        (append (myreverse (cdr list)) (first list))
+        (append (myreverse (cdr list)) (if (listp (first list)) 
+                                           (list (myreverse (first list))) 
+                                           (list (first list))))
     ) 
 )
 
-(myreverse '(a b c))     ; => (c b a)
-(myreverse '(a (b c) d)) ; => (d (c b) a)
+; (myreverse '(a b c))     ; => (c b a)
+; (myreverse '(a (b c) d)) ; => (d (c b) a)
 
